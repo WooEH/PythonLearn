fb7ef494499d82b4aaa25f082b70fad38678b6fc
WooEH <soubyt5292@gmail.com>
2020-09-08 15:59:14 +0900

    ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ

---
+++ 실습7.py
@@ -0,0 +1,12 @@
+# print("Start Ball-Dice Game")
+# print("Input")
+# dice1 = int(input())
+# dice2 = int(input())
+# print("Game Result")
+# if dice1 > dice2 :
+#     print("1st User Win !")
+# elif dice1 < dice2 :
+#     print("2nd User Win !")
+# else :
+#     print("Draw")
+# print("Stop Ball-Dice Game")
