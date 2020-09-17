4c4ba87c68b66bc6c24cfcebced7d65b11af62b7
WooEH <soubyt5292@gmail.com>
2020-09-17 19:10:11 +0900

    ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ

--- ex46.py
+++ ex46.py
@@ -1,7 +1,7 @@
 num = int(input("num = "))
 res = 0
 
-for i in range(num+1):
-    res = num * i
+for i in range(num):
+    res += num
 
 print("Result = %d" % res)
---
+++ ex47.py
@@ -0,0 +1,7 @@
+min = 0
+hour = 0
+
+for hour in range(24):
+    for min in range(60):
+        print("%02d:%02d" %(hour, min))
+
